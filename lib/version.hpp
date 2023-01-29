e3174eef735b3d3d3c88a06bed3e1f68c9cc65a1
langonginc <langonginc@yeah.net>
2023-01-29 13:13:56 +0800

    update to 6.0.16beta

--- lib/version.hpp
+++ lib/version.hpp
@@ -3,9 +3,9 @@
 
 void version_output ()
 {
-    printf ("Zff Version 6.0.1 Beta\nMade by ohzff 5+1 <https://github.com/ohzff/Zff-Reload>.\n");
+    printf ("Zff Version 6.0 Beta\nMade by ohzff 5+1 <https://github.com/ohzff/Zff-Reload>.\n");
     printf ("\nEnvironment information:\n");
-    printf ("Zff version              6.0.14\n");
+    printf ("Zff version              6.0.16beta\n");
     printf ("Data path                %susr\n", DATAPATH);
 }
 
